3af39d0e7e2e81f9939048bcb7180ad4f2e387f1
vp <vp>
2026-02-03 20:08:05 +0100

    remove old tui (opentui)

--- tools/bdk-tui/src/ui/dialogs/index.ts
+++
@@ -1,7 +0,0 @@
-// Dialog exports - centralized import point
-export * from './ModuleDialog.js';
-export * from './DbContextDialog.js';
-export * from './TextInputDialog.js';
-export * from './ConfirmDialog.js';
-export * from './ProcessSelectDialog.js';
-export * from './RidDialog.js';
